19bf4e1150f38d55a585942b06f7d425e8df9d77
Ethan Kerzner <ethan.kerzner@gmail.com>
2016-10-14 14:17:03 -0600

    Remove dead code.

--- src/app/components/connectivityMatrixView/rows/cmControlRow.js
+++ src/app/components/connectivityMatrixView/rows/cmControlRow.js
@@ -58,35 +58,6 @@ export class cmControlRow extends cmMatrixRow {
             self.onRollupColClicked(d3.select(this.parentNode.parentNode.parentNode.parentNode));
           });
 
-        //this.unrollControls[i].append("text")
-        //  .text("+")
-        //  .style("text-anchor", "start")
-        //  .style("dominant-baseline", "hanging");
-        //
-        //this.unrollControls[i].append("rect")
-        //  .attr("width", colWidth)
-        //  .attr("height", rowHeight)
-        //  .style("fill", "transparent")
-        //  .on("click", function () {
-        //    self.onUnrollColClicked(d3.select(this.parentNode.parentNode));
-        //  });
-        //
-        //this.rollupControls[i] = group.append("g")
-        //  .style("display", "none");
-        //
-        //this.rollupControls[i]
-        //  .append("text")
-        //  .text("-")
-        //  .style("text-anchor", "start")
-        //  .style("dominant-baseline", "hanging");
-        //
-        //this.rollupControls[i].append("rect")
-        //  .attr("width", colWidth)
-        //  .attr("height", rowHeight)
-        //  .style("fill", "transparent")
-        //  .on("click", function () {
-        //    self.onRollupColClicked(d3.select(this.parentNode.parentNode));
-        //  });
       }
     }
 
@@ -95,7 +66,6 @@ export class cmControlRow extends cmMatrixRow {
 
   onUnrollColClicked(majorCol) {
     var colIndex = cmMatrixRow.getMajorCellIndex(majorCol);
-    console.error(colIndex, majorCol);
     this.unrollControls[colIndex].style("display", "none");
     this.rollupControls[colIndex].style("display", "block");
     this.colClickedCallback(colIndex, true);
